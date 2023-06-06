68313866e4cd76ca589af819913666c543706ab4
largenumberhere <largeNumbergoeshere@gmail.com>
2023-06-06 22:18:07 +1000

    Added dad joke command

--- docker/docker_build_and_run.sh
+++ docker/docker_build_and_run.sh
@@ -2,7 +2,7 @@
 
 echo Building Dockerfile from github repo
 cd .. # facts-bot/
-docker build -t factsbot -f docker/Dockerfile .
+docker build --nocache -t factsbot -f docker/Dockerfile .
 echo Running container as factsbotcontainer with --rm
 echo Warning: You must include keys within this folder. You must make a subdirectory called keys, it must contain a file for each key required. Read github readme for info on what keys are quired
 sudo docker run --name factsbotcontainer --rm -it factsbot
